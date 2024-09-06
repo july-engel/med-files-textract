a58e8b590f3007f7d770bb3d36748664257fd0bd
julyengel <july.engel@medprevonline.com>
2024-09-06 17:31:59 -0300

    feat: Update dependencies for AWS SDK and add client-bedrock-runtime

--- src/textract.ts
+++ src/textract.ts
@@ -2,12 +2,11 @@ import {
   TextractClient,
   DetectDocumentTextCommand,
 } from "@aws-sdk/client-textract";
-import {
-  Comprehend,
-  DetectEntitiesCommandOutput,
-  LanguageCode,
-} from "@aws-sdk/client-comprehend";
 import fs from "fs";
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from "@aws-sdk/client-bedrock-runtime";
 
 async function detectTextFromImage(
   imagePath: string
@@ -36,31 +35,43 @@ async function detectTextFromImage(
   }
 }
 
-async function detectEntitiesFromText(
-  text: string,
-  languageCode: LanguageCode = "pt"
-): Promise<DetectEntitiesCommandOutput> {
-  const instancyComprehendClient = new Comprehend({
-    region: "us-east-1",
-  });
-
-  const responseEntities = await instancyComprehendClient.detectEntities({
-    Text: text,
-    LanguageCode: languageCode,
-  });
-
-  return responseEntities;
-}
-
 async function main(imagePath: string) {
   const responseText = await detectTextFromImage(imagePath);
+
+  const AWS_REGION = "us-east-1";
+
+  const MODEL_ID = "amazon.titan-text-express-v1";
+
+  const PROMPT = `Hi. please stract crm number from${responseText}`;
+
+  const payload = {
+    inputText: PROMPT,
+    textGenerationConfig: {
+      maxTokenCount: 4096,
+      stopSequences: [],
+      temperature: 0,
+      topP: 1,
+    },
+  };
   if (!responseText) {
     console.error("Erro ao detectar texto na imagem.");
     return;
   }
 
-  const responseEntities = await detectEntitiesFromText(responseText);
-  console.log(responseEntities);
+  const client = new BedrockRuntimeClient({ region: AWS_REGION });
+
+  const apiResponse = await client.send(
+    new InvokeModelCommand({
+      contentType: "application/json",
+      body: JSON.stringify(payload),
+      modelId: MODEL_ID,
+    })
+  );
+
+  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+  const responseBody = JSON.parse(decodedResponseBody);
+
+  console.log(responseBody);
 }
 
 const imagePath = process.argv[2];
